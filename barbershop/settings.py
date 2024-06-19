5ecf8d0b89a5ba095fc54f5b6614e381df5b7fd9
Luis Monteiro <luismonteirodeveloper@gmail.com>
2024-06-19 17:42:27 -0300

    fix: Comentarios redundantes removidos

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -29,13 +29,18 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8080',  # Substitua pelo endereço do seu frontend Vue.js
-    'http://127.0.0.1:8080',  # Se estiver usando localhost
-    # Adicione outros domínios ou IPs que você deseja permitir
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
 ]
-
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
+CORS_ALLOW_METHODS = [
+    'GET', 
+    'POST', 
+    'PUT', 
+    'PATCH', 
+    'DELETE', 
+    'OPTIONS'
+]
 CORS_ALLOW_HEADERS = [
     'Accept',
     'Accept-Encoding',
