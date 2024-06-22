4d1ccab9fdb63ba6c9588bfe07d3b68f047690d3
Luis Monteiro <luismonteirodeveloper@gmail.com>
2024-06-22 19:00:24 -0300

    fix: Correção imports dos signals

--- barbearias/signals/__init__.py
+++ barbearias/signals/__init__.py
@@ -1,14 +1,10 @@
 from .signal_atualizar_avaliacao_barbeiro import atualizar_avaliacao
 from .signal_registrar_cliente import registrar_cliente
 from .signal_registrar_funcionario import registrar_funcionario
-from .signal_registrar_lucros_barbearia import (
-    # atualizar_lucros,
-    registrar_lucros,
-)
+from .signal_registrar_lucros_barbearia import registrar_lucros
 
 __all__ = [
     "atualizar_avaliacao",
-    # 'atualizar_lucros',
     "registrar_cliente",
     "registrar_funcionario",
     "registrar_lucros",
