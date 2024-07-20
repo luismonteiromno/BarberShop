eee50622c814233a8c3443ba9e4a060b571084c0
Luis Monteiro <luismonteirodeveloper@gmail.com>
2024-07-20 18:24:00 -0300

    fix2

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'vercel.app']
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
